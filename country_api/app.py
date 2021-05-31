c87a7db2d7e3463a7d51dac8958822cc497c9f63
Jayden <jaydenlsf47@gmail.com>
2021-05-31 00:30:12 +0000

    Modified

--- country_api/app.py
+++ country_api/app.py
@@ -7,19 +7,19 @@ import json
 app = Flask(__name__)
 
 
-@app.route("/")
 @app.route("/get_country", methods=["GET"])
-def get_country_code():
+def get_country():
     country_url = "http://country.io/names.json"
-    countries = json.loads(requests.get(country_url).text)
+    response = requests.get(country_url)
+    countries = json.loads(response.text)
     letters = string.ascii_uppercase
     while True:
         random_country_code = "".join(random.choice(letters) for i in range(2))
         if random_country_code not in countries:
             continue
         else:
-            return random_country_code, countries[random_country_code]
+            return f"{random_country_code}-{countries[random_country_code]}"
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
--- server/app.py
+++ server/app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    country_code = requests.get("http://country_api:5000/get_country_code")
-    return render_template("index.html", country_code=country_code)
+    country_response = requests.get("http://country_api:5001/get_country").text
+    country_code = country_response.split('-')[0]
+    country_name = country_response.split('-')[1]
+
+    population_response = requests.get("http://population_api:5001/get_population").text
+    return render_template("index.html", country_code=country_code, country_name=country_name)
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
