1537b9ad5b2f160256160994735c077832c64656
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-03 15:24:39 +0000

    Modified

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class CovidStats(db.Model):
