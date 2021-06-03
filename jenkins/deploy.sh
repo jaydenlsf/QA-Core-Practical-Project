a1a63928b2e753f957817395aac7c7e16e289e6b
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-03 16:18:14 +0000

    Modified

--- jenkins/deploy.sh
+++
@@ -1,8 +0,0 @@
-scp docker-compose.yaml {machine name}:
-ssh {machine name}} << EOF
-
-export DATABASE_URI=${DATABASE_URI}
-docker stack deploy --compose-file docker-compose.yaml covid-19-app
-docker exec covid-19-app python create.py
-
-EOF
--- jenkins/setup-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl https://get.docker.com | sudo bash
-
-sudo usermod -aG docker jenkins
-
-
-docker login -u jaydenlsf -p Iwanttogym99!
