0c455ba98c7fec642d40084b6d89e1004691cf0d
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-04 19:06:59 +0000

    Modified

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -3,6 +3,5 @@ ssh swarm-master << EOF
 
 export DATABASE_URI=${DATABASE_URI}
 docker stack deploy --compose-file docker-compose.yaml covid-19-app
-# docker exec covid-19-app python create.py
 
 EOF
