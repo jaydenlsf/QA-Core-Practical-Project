97fa690bba41ee0cb431955390b1fe5a9d48b757
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-04 17:54:24 +0000

    Modified

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -2,8 +2,7 @@ scp docker-compose.yaml swarm-master:
 ssh swarm-master << EOF
 
 export DATABASE_URI=${DATABASE_URI}
-# docker run -d -p 80:80 --name nginx --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
 docker stack deploy --compose-file docker-compose.yaml covid-19-app
-docker exec covid-19-app_covid-19-app python create.py
+# docker exec covid-19-app python create.py
 
 EOF
