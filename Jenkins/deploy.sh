bdca99f55507627062fe59d6d1dab901752b3c64
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-04 15:06:11 +0000

    Modified

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -2,6 +2,7 @@ scp docker-compose.yaml docker-manager:
 ssh docker-manager << EOF
 
 export DATABASE_URI=${DATABASE_URI}
+docker run -d -p 80:80 --name nginx --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
 docker stack deploy --compose-file docker-compose.yaml covid-19-app
 docker exec covid-19-app python create.py
 
