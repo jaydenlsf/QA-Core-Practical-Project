2f18217c7013020895f028491b5d07bfec9a8775
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-05 11:39:33 +0000

    Modified

--- population_api/tests/test_population_api.py
+++ population_api/tests/test_population_api.py
@@ -1,5 +1,6 @@
 from flask import url_for
 from flask_testing import TestCase
+import json
 import requests_mock
 from app import app
 
@@ -8,24 +9,29 @@ class TestBase(TestCase):
         return app
 
 class TestPopulationAPI(TestBase):
-    def test_population_api(self):
-        response = self.client.post(url_for('get_population'))
-        self.assertEqual(response.status_code, 200)
+    # def test_population_api(self):
+    #     response = self.client.post(url_for('get_population'))
+    #     self.assertEqual(response.status_code, 200)
 
-    def test_uk_population(self):
+    # def test_uk_population(self):
 
-        response = self.client.post(url_for('get_population'), data='gb').data.decode('utf-8')
-        population = int(response.replace(',', ''))
-        self.assertTrue(population > 65000000)
+    #     response = self.client.post(url_for('get_population'), data='gb').data.decode('utf-8')
+    #     population = int(response.replace(',', ''))
+    #     self.assertTrue(population > 65000000)
 
-    def test_us_population(self):
-        response = self.client.post(url_for('get_population'), data='us').data.decode('utf-8')
-        population = int(response.replace(',', ''))
-        self.assertTrue(population > 320000000)
+    # def test_us_population(self):
+    #     response = self.client.post(url_for('get_population'), data='us').data.decode('utf-8')
+    #     population = int(response.replace(',', ''))
+    #     self.assertTrue(population > 320000000)
+
+    # def test_get_population(self):
+    #     with requests_mock.Mocker() as mocker:
+    #         mocker.post('http://covid-19-app:5000/get_population',json={"country_code":'uk'})
+
+    #     response = self.client.post(url_for('get_population', country_code="uk"))
+    #     self.assertEqual(response.status_code, 200)
 
     def test_get_population(self):
-        with requests_mock.Mocker() as mocker:
-            mocker.post('http://covid-19-app:5000/get_population',json={"country_code":'uk'})
-
-        response = self.client.post(url_for('get_population', country_code="uk"))
+        response = self.client.post(url_for('get_population'), data='gb')
+        response_data = response.data.decode('utf-8')
         self.assertEqual(response.status_code, 200)
