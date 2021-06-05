c8a8b226e544a2ff286deecfa41f4f5d22a1bba7
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-05 11:42:50 +0000

    Modified

--- population_api/tests/test_population_api.py
+++ population_api/tests/test_population_api.py
@@ -9,29 +9,9 @@ class TestBase(TestCase):
         return app
 
 class TestPopulationAPI(TestBase):
-    # def test_population_api(self):
-    #     response = self.client.post(url_for('get_population'))
-    #     self.assertEqual(response.status_code, 200)
-
-    # def test_uk_population(self):
-
-    #     response = self.client.post(url_for('get_population'), data='gb').data.decode('utf-8')
-    #     population = int(response.replace(',', ''))
-    #     self.assertTrue(population > 65000000)
-
-    # def test_us_population(self):
-    #     response = self.client.post(url_for('get_population'), data='us').data.decode('utf-8')
-    #     population = int(response.replace(',', ''))
-    #     self.assertTrue(population > 320000000)
-
-    # def test_get_population(self):
-    #     with requests_mock.Mocker() as mocker:
-    #         mocker.post('http://covid-19-app:5000/get_population',json={"country_code":'uk'})
-
-    #     response = self.client.post(url_for('get_population', country_code="uk"))
-    #     self.assertEqual(response.status_code, 200)
-
     def test_get_population(self):
         response = self.client.post(url_for('get_population'), data='gb')
         response_data = response.data.decode('utf-8')
+        population = int(json.loads(response_data)['population'].replace(',', ''))
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(population > 0)
