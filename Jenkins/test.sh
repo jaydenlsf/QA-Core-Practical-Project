c8fc63d62d3e7e76c7bafc7badb1fbebc0800ca2
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-06 17:22:04 +0000

    Removed comments

--- Jenkins/test.sh
+++ Jenkins/test.sh
@@ -12,11 +12,3 @@ for service in country_api population_api stats_api server
 do
 python3 -m pytest $service --cov=$service --cov-report=xml --junitxml=junit/test-results.xml
 done
-
-# python3 -m pytest country_api --cov=country_api --cov-report=xml --junitxml=junit/test-results.xml
-
-# python3 -m pytest population_api --cov=population_api --cov-report=xml --junitxml=junit/test-results.xml
-
-# python3 -m pytest stats_api --cov=stats_api --cov-report=xml --junitxml=junit/test-results.xml
-
-# python3 -m pytest server --cov=server --cov-report=xml --junitxml=junit/test-results.xml
