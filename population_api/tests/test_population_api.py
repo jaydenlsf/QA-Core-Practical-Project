4729a6a59626975cf8eb77e4d9284a063bee0202
Jayden Lee <jaydenlsf47@gmail.com>
2021-06-06 16:02:10 +0000

    Additional test

--- population_api/tests/test_population_api.py
+++ population_api/tests/test_population_api.py
@@ -13,5 +13,7 @@ class TestPopulationAPI(TestBase):
         response = self.client.post(url_for('get_population'), data='gb')
         response_data = response.data.decode('utf-8')
         population = int(json.loads(response_data)['population'].replace(',', ''))
+        alpha3code = json.loads(response_data)['alpha3code']
         self.assertEqual(response.status_code, 200)
         self.assertTrue(population > 0)
+        self.assertEqual(len(alpha3code), 3)
